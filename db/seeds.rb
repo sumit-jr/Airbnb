90a421dff1e5740767a6f14d51b11a9ea3e1137a
sumit-jr <er.sahsumit@gmail.com>
2024-12-30 06:41:24 +0545

    feat: add Property model, validations, and dynamic property listing page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Property.create!({
+  name: "The White House",
+  headline: "The White House",
+  description: "The White House is the official residence and workplace of the president of the United States. It is located at 1600 Pennsylvania Avenue NW in Washington, D.C., and has been the residence of every U.S. president since John Adams in 1800.",
+  address_1: "1600 Pennsylvania Avenue NW",
+  address_2: "Washington, DC 20500, USA",
+  city: "Washington",
+  state: "DC",
+  country: "USA"
+
+})
+
+Property.create!({
+  name: "Buckingham Palace",
+  headline: "Buckingham Palace",
+  description: "Buckingham Palace is the London residence and administrative headquarters of the monarch of the United Kingdom. Located in the City of Westminster, the palace is often at the centre of state occasions and royal hospitality.",
+  address_1: "Westminster",
+  address_2: "London SW1A 1AA, UK",
+  city: "London",
+  state: "England",
+  country: "UK"
+})
