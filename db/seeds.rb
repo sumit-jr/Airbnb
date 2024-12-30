ef1c376d11bff7c027be3f1e3c40cf287c39d54d
sumit-jr <er.sahsumit@gmail.com>
2024-12-30 08:11:52 +0545

    Add Active Storage, image processing gem, and migration for property images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-20.times do
-  Property.create!({
+6.times do |i|
+  property = Property.create!({
     name: Faker::Company.name,
     headline: Faker::Company.catch_phrase,
     description: Faker::Company.bs,
@@ -10,4 +10,7 @@
     country: Faker::Address.country,
     price: Money.from_amount(rand(50..100), 'USD')
   })
+
+  property.images.attach(io: File.open("db/images/property_#{i + 1}.png"), filename: "propety.name")
+  property.images.attach(io: File.open("db/images/property_#{i + 7}.png"), filename: "propety.name")
 end
